14c47fe21ca1384a8442b5e6c89498b954c5fdce
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-24 20:57:37 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,1 +1,21 @@
-<?php // Boot code
+<?php
+/**
+ * Media Manager Helper
+ *
+ * @author KLXM Crossmedia GmbH
+ * @author Thomas Skerbis
+ */
+
+// Effect für Media Manager registrieren
+if (rex::isBackend() && rex::getUser()) {
+    rex_media_manager::addEffect('rex_effect_srcset_helper');
+}
+
+// Falls Frontend: Output-Filter für srcset-Verarbeitung registrieren
+if (rex::isFrontend()) {
+    rex_extension::register('OUTPUT_FILTER', function (rex_extension_point $ep) {
+        $content = $ep->getSubject();
+        $content = \KLXM\MediaManagerHelper\ResponsiveImage::replaceMediaTags($content);
+        $ep->setSubject($content);
+    });
+}
